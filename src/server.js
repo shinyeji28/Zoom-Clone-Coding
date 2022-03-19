58d3ff2cc18e793f5bec281711bab60b1836f2cf
shinyeji28 <xntm10@naver.com>
2022-03-19 21:02:59 +0900

    front setup

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
 import express from "express";
+
 const app = express();
-console.log("hello")
-app.listen(3000);
+
+app.set("view engine","pug");                             // pug를 view engine으로 설정
+app.set("views",__dirname + "/views");                    // express template의 위치 지정
+
+app.use("/public",express.static(__dirname + "/public")); // public url을 생성하여 유저에게 파일 공유 (정적 파일 제공)
+
+app.get("/",(req, res) => res.render("home"));            // home.pug를 render 해주는 route handler를 만듦
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000,handleListen);
