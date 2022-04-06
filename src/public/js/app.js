04c0a1655146699984a976a2b9909a5d4fb2d4e0
shinyeji28 <xntm10@naver.com>
2022-04-06 14:59:38 +0900

    send nickname msg to server (with stringify)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,36 @@
+const meassageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
 const socket = new  WebSocket(`ws://${window.location.host}`); // ws://localhost:3000로 해도 되지만 모바일은 localhost를 모르기 때문에 브라우저가 알 수 있는 js 명령어로 url을 줌
                                                                 // front의 socket (서버와 연결을 위한 소켓)
+function makeMessage(type,payload){  // backend로 보낼 때 json object를 String 형태로 만들어 보내고 backend에서 파싱해야 함
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
 socket.addEventListener("open",() => {  // 소켓연결되면 실행
     console.log("Connected to Server");
 });
 socket.addEventListener("message", (message) => {   // server에서 "send"한 메세지
-    console.log("New message: ",message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    meassageList.append(li);
 });
 socket.addEventListener("close", () => {      // 서버에서 통신이 끊기면
     console.log("Disconnected from Server");
 });
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
