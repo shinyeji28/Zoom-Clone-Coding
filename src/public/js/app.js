53bb12b8eb1148b71ab33875483683fc37e4bbc3
shinyeji28 <xntm10@naver.com>
2022-04-07 14:33:48 +0900

    socketio.emit(event, arguments..., function)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,14 @@ const socket = io();  // io : 자동적으로 backend와  socket.io를 연결해
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 
+function backendDone(msg){
+    console.log(`The backend says: `, msg);
+}
+
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("server is done!");
-  });  // socket.emit argument : (event name, contents, 서버에서 function을 호출하면 frontend에서 실행되는 function)
+  socket.emit("enter_room", { payload: input.value }, backendDone );  // argument 수는 제한이 없음.  단, function을 보낼 땐 마지막에!! (서버에서 function을 호출하면 frontend에서 실행되는 function)
   input.value = "";
 }
 
