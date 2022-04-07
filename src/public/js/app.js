7e9ae369a1c1343a3beeaa30bda2509233a9899f
shinyeji28 <xntm10@naver.com>
2022-04-07 12:50:06 +0900

    use socketIO communication(with the socketIO.emit/on)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,15 @@
-const socket = io();  // io : 자동적으로 backend와  socket.io를 연결해주는 function, 알아서 socket.io를 실행하고 잇는 서버를 찾을 것임
+const socket = io();  // io : 자동적으로 backend와  socket.io를 연결해주는 function, 알아서 socket.io를 실행하고 있는 서버를 찾을 것임
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });  // socket.emit argument : (event name, contents, 서버에서 function을 호출하면 frontend에서 실행되는 function)
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
