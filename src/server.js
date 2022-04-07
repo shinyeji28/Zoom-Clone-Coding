d2c587eaa741759e93d30b335a508e7c6d091728
shinyeji28 <xntm10@naver.com>
2022-04-07 12:03:12 +0900

    Use SocketIO

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
-import WebSocket from "ws";  //websockket :브라우저와 서버사이의 연결
 import { disconnect } from "process";
 
 const app = express();
@@ -15,29 +15,35 @@ app.get("/*",(req, res)=> res.redirect("/"));             // 유저가 어떤 ur
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];  // fack DB - 연결된 소켓의 array 만들기 (메세지를 다른 소켓에 전달해 주기 위해..)
+wsServer.on("connection",(socket) => {
+    console.log(socket);
+});
 
-wss.on("connection",(socket) => {   // server의 socket (브라우저와 연결을 위한 소켓)
-                                    // socket이 frontend와 real time으로 소통할 수 있음
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from the Browser"));                         // 브라우저가 종료되면
-    socket.on("message", (msg) => {
-    const message = JSON.parse(msg);// frontend로 부터 받은 string을 javaScript object로 변경
-        switch(message.type){
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload.toString('utf8')}`));
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;  // socket은 객체임으로 이와같은 코딩이 가능 => socket안에 정보를 저장 할 수 있음
-                break;
-        }
-    });
+// const wss = new WebSocket.Server({server});
+
+// const sockets = [];  // fack DB - 연결된 소켓의 array 만들기 (메세지를 다른 소켓에 전달해 주기 위해..)
+
+// wss.on("connection",(socket) => {   // server의 socket (브라우저와 연결을 위한 소켓)
+//                                     // socket이 frontend와 real time으로 소통할 수 있음
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser");
+//     socket.on("close", () => console.log("Disconnected from the Browser"));                         // 브라우저가 종료되면
+//     socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);// frontend로 부터 받은 string을 javaScript object로 변경
+//         switch(message.type){
+//             case "new_message":
+//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload.toString('utf8')}`));
+//                 break;
+//             case "nickname":
+//                 socket["nickname"] = message.payload;  // socket은 객체임으로 이와같은 코딩이 가능 => socket안에 정보를 저장 할 수 있음
+//                 break;
+//         }
+//     });
     
-});  // frontend랑 backend랑 connect 될 때마다 작동
+// });  // frontend랑 backend랑 connect 될 때마다 작동
 
-server.listen(3000,handleListen);
+httpServer.listen(3000,handleListen);
