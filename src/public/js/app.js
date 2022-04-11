1ec156a24256d632a4fabd6002f68ab46fff7dcc
shinyeji28 <xntm10@naver.com>
2022-04-11 15:55:15 +0900

    show the video using the navigator.mediaDevices.getUserMedia

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,75 +1,43 @@
 const socket = io();  // io : 자동적으로 backend와  socket.io를 연결해주는 function, 알아서 socket.io를 실행하고 있는 서버를 찾을 것임
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-room.hidden = true;
+let myStream;  // stream : video와 audio가 결합된 것
+let muted = false;
+let cameraOff = false;
+async function getMedia(){
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia({    //navigator.mediaDevices.getUserMedia: user media의 string을 줌
+            audio: true,
+            video: true
+        });
+        myFace.srcObject =  myStream;
+    } catch (e) {
+        console.log(`video error - ${e}`);
+    }
+}
+getMedia();
 
-let roomName;
-
-function addMessage(message){
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
+function handleMuteClick(){
+    if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else{
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+function handleCameraClick(){
+    if(cameraOff){
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else{
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
 }
 
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message",input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value="";
-}
-
-function handleNicknameSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    socket.emit("nickname",input.value);
-}
-
-function showRoom(){
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`
-    const msgFrom = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
-    msgFrom.addEventListener("submit",handleMessageSubmit);
-    nameForm.addEventListener("submit",handleNicknameSubmit);
-
-}
-
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value , showRoom );  // argument 수는 제한이 없음.  단, function을 보낼 땐 마지막에!! (서버에서 function을 호출하면 frontend에서 실행되는 function)
-  roomName = input.value;
-  input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome",(user, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${user} arrived!`);
-});
-socket.on("bye",(left, newCount)=> {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${left} left ㅠㅠ`);
-});
-socket.on("new_message",addMessage);
-socket.on("room_change",(rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML="";
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    });
-});
+muteBtn.addEventListener("click",handleMuteClick);
+cameraBtn.addEventListener("click",handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import {Server} from "socket.io";
+import SocketIO from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
@@ -16,83 +16,7 @@ app.get("/*",(req, res)=> res.redirect("/"));             // 유저가 어떤 ur
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer,  {   
-  cors: {                                           //Admin-ui (온라인 데모가 작동할 수 있도록 환경 설정)
-    origin: ["https://admin.socket.io"],
-    credentials: true
-  }
-});
-instrument(wsServer, {
-  auth: false                                       // Admin-ui (패스워드 false)  
-});
+const wsServer = SocketIO(httpServer);  
 
-function publicRooms(){         // public room만 반환하는 함수
-  const {
-    sockets: {
-      adapter: {sids, rooms}
-    }
-  } = wsServer;  // sids : socket ids , rooms : room names (socket 내부 구조 사용)
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if(sids.get(key) === undefined){
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName){
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection",(socket) => {
-  socket["nickname"]="Anon";
-  socket.onAny((event) => {                 // 어떤 이벤트이든 콘솔을 찍을 수 있음
-      console.log(`Socket Event:${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);      // join the room
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));  // 본인을 제외한 방에 있는 모든 사람에게 ~
-    wsServer.emit("room_change", publicRooms());            // 서버의 모든 브라우저에게 전달
-  });
-  socket.on("disconnecting",() => {
-      socket.rooms.forEach((room) => {
-        socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
-      })
-  });
-  socket.on("disconnect",() => {
-    wsServer.emit("room_change", publicRooms());
-  });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message",`${socket.nickname}: ${msg}`);
-    done();
-  })
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-// const wss = new WebSocket.Server({server});
-
-// const sockets = [];  // fack DB - 연결된 소켓의 array 만들기 (메세지를 다른 소켓에 전달해 주기 위해..)
-
-// wss.on("connection",(socket) => {   // server의 socket (브라우저와 연결을 위한 소켓)
-//                                     // socket이 frontend와 real time으로 소통할 수 있음
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser");
-//     socket.on("close", () => console.log("Disconnected from the Browser"));                         // 브라우저가 종료되면
-//     socket.on("message", (msg) => {
-//     const message = JSON.parse(msg);// frontend로 부터 받은 string을 javaScript object로 변경
-//         switch(message.type){
-//             case "new_message":
-//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload.toString('utf8')}`));
-//                 break;
-//             case "nickname":
-//                 socket["nickname"] = message.payload;  // socket은 객체임으로 이와같은 코딩이 가능 => socket안에 정보를 저장 할 수 있음
-//                 break;
-//         }
-//     });
-    
-// });  // frontend랑 backend랑 connect 될 때마다 작동
 
 httpServer.listen(3000,handleListen);
